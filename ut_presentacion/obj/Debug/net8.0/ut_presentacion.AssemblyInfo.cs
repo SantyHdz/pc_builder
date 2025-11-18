b9e47e36098151fa6aec3703a53437980021d026
Santiago Hernandez <trollearvpn156@gmail.com>
2025-11-18 18:38:18 -0500

    .\.git\

--- ut_presentacion/obj/Debug/net8.0/ut_presentacion.AssemblyInfo.cs
+++ ut_presentacion/obj/Debug/net8.0/ut_presentacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ut_presentacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8eb71cf8ed7ebccd60253a3b711def7637b758e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+119bdf67aedfcb0156a163626cda6defee868b72")]
 [assembly: System.Reflection.AssemblyProductAttribute("ut_presentacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ut_presentacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
